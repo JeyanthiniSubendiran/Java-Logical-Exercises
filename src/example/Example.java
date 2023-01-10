a265e9e0e05eb1ffad757de4253d51b280557108
Jeyanthini <jeyanthinisubendiran@gmail.com>
2023-01-10 14:00:40 +0530

    array code commit

--- src/example/Example.java
+++ src/example/Example.java
@@ -4,7 +4,16 @@ public class Example {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		int[] FirstArray =new int[10];
+		FirstArray[0]=1;
+		FirstArray[1]=2;
+		FirstArray[2]=3;
+		FirstArray[3]=4;
+		
+		System.out.println(FirstArray[0]);
+		int[] FirstArray1 = {4,5,6};
+		System.out.println(FirstArray1.length);
 
 	}
-
+public void demo() {}
 }
